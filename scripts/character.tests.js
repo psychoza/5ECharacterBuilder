072f37e02a73842acc94154a78cf8f66951fe68b
j.w.hutchison@live.com <dragon26>
2014-07-09 17:14:00 -0400

    Added more properties and associated tests and fixed some other properties

--- scripts/character.tests.js
+++ scripts/character.tests.js
@@ -46,6 +46,54 @@ describe ('character', function(){
 		it('must have initiative', function(){
 			expect(character.initiative != undefined).toBe(true);
 		});
+		it('must have speed', function(){
+			expect(character.speed != undefined).toBe(true);
+		});
+		it('must have maxHitPoints', function(){
+			expect(character.maxHitPoints != undefined).toBe(true);
+		});
+		it('must have currentHitPoints', function(){
+			expect(character.currentHitPoints != undefined).toBe(true);
+		});
+		it('must have tempHitPoints', function(){
+			expect(character.tempHitPoints != undefined).toBe(true);
+		});
+		it('must have personalityTraits', function(){
+		    expect(character.personalityTraits != undefined).toBe(true);
+		});
+		it('must have ideals', function(){
+		    expect(character.ideals != undefined).toBe(true);
+		});
+		it('must have bonds', function(){
+		    expect(character.bonds != undefined).toBe(true);
+		});
+		it('must have flaws', function(){
+		    expect(character.flaws != undefined).toBe(true);
+		});
+		it('must have age', function(){
+		    expect(character.age != undefined).toBe(true);
+		});+
+		it('must have height', function(){
+		    expect(character.height != undefined).toBe(true);
+		});
+		it('must have weight', function(){
+		    expect(character.weight != undefined).toBe(true);
+		});
+		it('must have eyes', function(){
+		    expect(character.eyes != undefined).toBe(true);
+		});
+		it('must have skin', function(){
+		    expect(character.skin != undefined).toBe(true);
+		});
+		it('must have hair', function(){
+		    expect(character.hair != undefined).toBe(true);
+		});
+		it('must have totalHitDice', function(){
+		    expect(character.totalHitDice != undefined).toBe(true);
+		});
+		it('must have currentHitDice', function(){
+		    expect(character.currentHitDice != undefined).toBe(true);
+		});
 	});
 });
 
